6637210d805d21c5915e5c4dc89da090404c732a
NebiyouChanie <cnebiyu@gmail.com>
2024-12-25 15:09:43 +0300

    Project initalization

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <p>pharma Connect</p>
+      <Button>Start</Button>
+    </div>
+  )
+}
+
+export default Home
