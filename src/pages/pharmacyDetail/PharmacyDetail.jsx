9d566d34cdb78c2c670fe744fc7a44eabec3fde9
Yetmgeta Ewunetu <yetmgetae@gmail.com>
2024-12-28 23:41:33 +0300

    commiting PharmacyDetail page

--- src/pages/pharmacyDetail/PharmacyDetail.jsx
+++ src/pages/pharmacyDetail/PharmacyDetail.jsx
@@ -1,13 +1,47 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 import medicineImage from "../../assets/medicine.png";
 import { Clock } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+function MapClickHandler({ setCoordinates }) {
+  useMapEvents({
+    click: (e) => {
+      const { lat, lng } = e.latlng;
+      setCoordinates({ lat, lng });
+    },
+  });
+  return null;
+}
+function AutoFocusMarker({ position }) {
+  const map = useMap();
+
+  if (position) {
+    map.setView(position, map.getZoom()); // Update the map view to focus on the marker
+  }
+
+  return null;
+}
 export default function PharmacyDetail() {
+  const [coordinates, setCoordinates] = useState({
+    lat: 8.989,
+    lng: 38.74,
+  });
+  console.log(coordinates);
   return (
-    <div>
-      <div className="container flex items-center flex-col gap-8 ">
-        <div className="md:w-3/4 mt-8 text-gray-700 md:flex md:justify-center  md:gap-8">
+    <div className="container mb-8">
+      <div className="flex items-center flex-col gap-8 mb-5">
+        <div className="mt-8 w-full text-gray-700 md:flex md:justify-center md:gap-8">
           <div className="my-7 md:my-0 md:w-2/4">
             <h3 className="text-2xl md:text-3xl mb-5 font-bold text-black">
               Lion Pharmacy
@@ -15,11 +49,11 @@ export default function PharmacyDetail() {
             <p>Location: Addisu Gebeya</p>
             <p>Owner: Abebe Kebede</p>
             <p className="flex items-center gap-4 mb-8">
-              Destance: Around 4.6KM{" "}
-              <p className="flex gap-1 items-center font-semibold">
+              Distance: Around 4.6KM{" "}
+              <span className="flex gap-1 items-center font-semibold">
                 <Clock className="w-4 h-4" />
                 20Min
-              </p>
+              </span>
             </p>
           </div>
           <img
@@ -32,18 +66,37 @@ export default function PharmacyDetail() {
           center={[51.505, -0.09]}
           zoom={13}
           scrollWheelZoom={false}
-          style={{ width: "80%", height: "50vh" }}
+          style={{ width: "100%", height: "60vh" }}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker
+            position={[
+              coordinates.lat || 8.989590867391874,
+              coordinates.lng || 38.74367144599007,
+            ]}
+          >
+            <Popup>Name of the Pharmacy</Popup>
           </Marker>
+
+          <MapClickHandler setCoordinates={setCoordinates} />
+          <AutoFocusMarker
+            position={[
+              coordinates.lat || 8.989590867391874,
+              coordinates.lng || 38.74367144599007,
+            ]}
+          />
         </MapContainer>
+
+        <Button variant="outline" className="self-start">
+          Open in Google Maps
+        </Button>
+      </div>
+      <div className="flex w-full max-w-sm items-center mt-8">
+        <Input type="text" placeholder="Search from this pharmacy" />
+        <Button type="submit">Search</Button>
       </div>
     </div>
   );
