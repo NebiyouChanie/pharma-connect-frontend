d2340e082278e69865b4d45f431cfed5c3689ad5
NebiyouChanie <cnebiyu@gmail.com>
2025-01-13 23:24:22 +0300

    updated path

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -11,10 +11,7 @@ export function cn(...inputs) {
 //   baseURL: "http://localhost:5000/api/v1",  
 // });
 
-
-// export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
-
-export const  BASE_URL = "http://localhost:5000/api/v1"
+export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
 
 
 
