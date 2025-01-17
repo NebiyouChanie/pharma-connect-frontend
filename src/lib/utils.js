4af4fc868203189060357c81d8bc995697350459
NebiyouChanie <cnebiyu@gmail.com>
2025-01-17 16:31:01 +0300

    update base url

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,18 +1,13 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge"
-import axios from "axios";
-
+ 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
 
-// export const axiosInstance = axios.create({
-//   baseURL: "http://localhost:5000/api/v1",  
-// });
-
-// export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
-export const  BASE_URL = "http://localhost:5000/api/v1"
+export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
+// export const  BASE_URL = "http://localhost:5000/api/v1"
 
 
 
