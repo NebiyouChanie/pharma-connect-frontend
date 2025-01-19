a0ed8b1008c17051982c12e7fe4bc331a6460721
NebiyouChanie <cnebiyu@gmail.com>
2025-01-20 01:04:49 +0300

    to fixed

--- src/pages/pharmacyDetail/PharmacyDetail.jsx
+++ src/pages/pharmacyDetail/PharmacyDetail.jsx
@@ -235,9 +235,9 @@ export default function PharmacyDetail() {
             </div>
                         
             <div className="flex items-center gap-4 my-8">
-              <p className="font-medium text-gray-700">Distance :</p> Around {distance.toFixed(2)} KM{" "}
+              <p className="font-medium text-gray-700">Distance :</p> Around {Math.fround(distance)} KM{" "}
               <span className="flex gap-1 items-center font-semibold">
-                <Clock className="w-4 h-4" />~{distance.toFixed(0)*3}{" "}
+                <Clock className="w-4 h-4" />~{Number(Math.round(distance))*3}{" "}
                 Min
               </span>
             </div>
