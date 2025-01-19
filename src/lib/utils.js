c4a09e8d9688d51056f510737442e369107d7ed7
NebiyouChanie <cnebiyu@gmail.com>
2025-01-20 00:39:25 +0300

    final

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,8 +6,8 @@ export function cn(...inputs) {
 }
 
 
-// export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
-export const  BASE_URL = "http://localhost:5000/api/v1"
+export const  BASE_URL = "https://pharma-connect-backend.onrender.com/api/v1"
+// export const  BASE_URL = "http://localhost:5000/api/v1"
 
 
 
